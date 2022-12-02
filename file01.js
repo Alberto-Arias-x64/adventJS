88bb045c64f68d4aa5907bac12979df0bd52c0d4
x64 <alberto.arias.x64@gmail.com>
2022-12-02 09:39:08 -0500

    last change on file01

--- file01.js
+++ file01.js
@@ -1,10 +1,12 @@
+const wrapping = gifts => gifts.map(element => `${'*'.repeat(element.length+2)}\n*${element}*\n${'*'.repeat(element.length+2)}`)
+
+
 const gifts = ["book", "game", "socks"];
 const wrapped = wrapping(gifts);
 console.log(wrapped);
+
 /* [
      "******\n*book*\n******",
      "******\n*game*\n******",
      "*******\n*socks*\n*******"
    ] */
-
-wrapping = (gifts) => gifts.map(element => `${'*'.repeat(element.length+2)}\n*${element}*\n${'*'.repeat(element.length+2)}`)
